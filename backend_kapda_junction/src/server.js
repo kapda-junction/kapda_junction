2cea49251e398c86eaa64c117640c4515c3d9279
Parag jain <iesparagjain@selcom.net>
2026-04-08 22:29:03 +0530

    test

--- backend_kapda_junction/src/server.js
+++ backend_kapda_junction/src/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 connectDB()
   .then(() => {
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`🚀 Kapda Junction API running on port ${PORT}`);
 
       // Optional local keep-alive.
